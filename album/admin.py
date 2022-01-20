7f022dd505e5cafea4f68a17ad9c954a74de85ef
HernandoTW <hernando.17@hotmail.com>
2022-01-20 08:28:51 -0500

    cargue_modelos
    Cargue modelos track, artist, album

---
+++ album/admin.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from .models import Album
+
+# Register your models here.
+class AdminAlbum(admin.ModelAdmin):
+	list_display = ('id','name','artist')
+	list_filter = ('artist',)
+	search_fields = ('name',)
+
+admin.site.register(Album, AdminAlbum)
--- musicaha/settings.py
+++ musicaha/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Artist'
+    'Artist',
+    'album',
+    'track'
 ]
 
 MIDDLEWARE = [
